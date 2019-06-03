fa75f7bdb9475fbd61a915bfd7ec7d3a26d3c26b
shaf12 <shafzhr@gmail.com>
2019-06-03 18:56:50 +0300

    Removed debugging macro

--- Main.asm
+++ Main.asm
@@ -2,43 +2,6 @@
 %TITLE "Passwords Maneger"
 IDEAL
 MODEL small
-macro Print32 number
-local println, loopr
-
-    push eax
-    push edx
-    push ebx
-    push si
-    mov cx,0
-    mov eax,number
-    mov ebx, 10
- loopr:  
-    xor edx, edx
-    div ebx         ; eax <- eax/10, edx <- eax % 10
-    add dl, '0'     ; edx to ASCII
-    push dx
-    inc cx
-    cmp eax, 0
-    jnz loopr
-
-    println:
-    pop dx
-    mov ah, 2h
-    int 21h
-    dec cx
-    jnz println
-
-    mov dl, 13d     ;Carriage Return
-    mov ah, 2h
-    int 21h
-    mov dl, 10d     ;Line Feed
-    mov ah, 2h
-    int 21h
-
-    pop si
-    pop edx
-    pop eax
-endm Print32
 
 MACRO SHOW_MSG MSG
 	mov dx, offset MSG
